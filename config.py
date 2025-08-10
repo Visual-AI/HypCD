30c1a55ee57e2f052abf53cf386cf2726a5ded90
liuyuanpei <ypliubit@gmail.com>
2025-08-11 01:27:03 +0800

    update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # -----------------
 # DATASET ROOTS
 # -----------------
-cub_root = '/lustre1/g/stat_han/datasets/cub'
-car_root = '/lustre1/g/stat_han/datasets/cars'
-aircraft_root = '/lustre1/g/stat_han/datasets/fgvc-aircraft-2013b'
-cifar_10_root = '/lustre1/g/stat_han/datasets/cifar10'
-cifar_100_root = '/lustre1/g/stat_han/datasets/cifar100'
-imagenet_root = '/lustre1/g/stat_han/datasets/ImageNet/ILSVRC12'
+cub_root = '{DATA_PATH}/cub'
+car_root = '{DATA_PATH}/cars'
+aircraft_root = '{DATA_PATH}/fgvc-aircraft-2013b'
+cifar_10_root = '{DATA_PATH}/cifar10'
+cifar_100_root = '{DATA_PATH}/cifar100'
+imagenet_root = '{DATA_PATH}/imagenet'
 
 # OSR Split dir
 osr_split_dir = 'data/ssb_splits'
@@ -14,11 +14,11 @@ osr_split_dir = 'data/ssb_splits'
 # -----------------
 # OTHER PATHS
 # -----------------
-dino_pretrain_path = '/home/ypliu0/projects/HypCD/pretrained/dino_vitb16_pretrain.pth'
-dinov2_pretrain_path = '/home/ypliu0/projects/HypCD/pretrained/dinov2_vitb14_reg4_pretrain.pth'
+dino_pretrain_path = '{PRETRAINED_PATH}/dino_vitb16_pretrain.pth'
+dinov2_pretrain_path = '{PRETRAINED_PATH}/dinov2_vitb14_reg4_pretrain.pth'
 feature_extract_dir = 'osr_novel_categories/extracted_features_public_impl'
 
 # -----------------
 # OTHER PATHS
 # -----------------
-exp_root = '/home/ypliu0/projects/HypCD/dev_outputs'  # All logs and checkpoints will be saved here
+exp_root = '{PROJECT_PATH}/HypCD/dev_outputs'  # All logs and checkpoints will be saved here
